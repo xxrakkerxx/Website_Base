528b68fbb08186220455b88d0f7371ad5afff3fb
Pips <keylupet@gmail.com>
2020-10-10 12:05:41 +0800

    sample

---
+++ Arithmetic.php
@@ -0,0 +1,10 @@
+<?php
+$x = 10;
+$y = 10;
+
+echo "$x + $y=", ($x + $y),"<br>"; // Addition	$x + $y	Sum of $x and $y
+echo "$x - $y=",($x - $y),"<br>"; // Subtraction	$x - $y	Difference of $x and $y.
+echo "$x * $y=",($x * $y),"<br>"; // Multiplication	$x * $y	Product of $x and $y.
+echo "$x / $y=",($x / $y),"<br>"; // Division	$x / $y	Quotient of $x and $y
+echo "$x % $y=",($x % $y),"<br>"; // Modulus $x % $y	Remainder of $x divided by $y
+?>
